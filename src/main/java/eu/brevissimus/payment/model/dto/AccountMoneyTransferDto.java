1e3c741327c4e0b30ea359a88d0419a314c0e921
cstenkes <cs.tenkes@gmail.com>
2025-03-30 17:33:42 +0200

    PYM0002 - usage of NotNull, NotBlank, Size, PositiveOrZero

--- src/main/java/eu/brevissimus/payment/model/dto/AccountMoneyTransferDto.java
+++ src/main/java/eu/brevissimus/payment/model/dto/AccountMoneyTransferDto.java
@@ -1,15 +1,34 @@
 package eu.brevissimus.payment.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 public record AccountMoneyTransferDto(
-        @Schema(description = "Issue Date", example = "2025-02-12 12:30") LocalDateTime issueDate,
-        @Schema(description = "From Account Number", example = "12345678A") String fromAccountNumber,
-        @Schema(description = "To Account Number", example = "12345678B") String toAccountNumber,
-        @Schema(description = "Transfer amount", example = "1000") BigDecimal amount,
-        @Schema(description = "Transfer to currency", example = "CHF") String currency) {
+        @NotNull
+        @Schema(description = "Issue Date", requiredMode = REQUIRED, example = "2025-02-12 12:30")
+        LocalDateTime issueDate,
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "From Account Number", requiredMode = REQUIRED, example = "12345678A")
+        String fromAccountNumber,
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "To Account Number", requiredMode = REQUIRED, example = "12345678B")
+        String toAccountNumber,
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "Transfer amount", requiredMode = REQUIRED, example = "1000")
+        BigDecimal amount,
+        @NotBlank
+        @Size(max = 3)
+        @Schema(description = "Transfer to currency", requiredMode = REQUIRED, example = "CHF")
+        String currency) {
 
 }
--- src/main/java/eu/brevissimus/payment/model/dto/CardDto.java
+++ src/main/java/eu/brevissimus/payment/model/dto/CardDto.java
@@ -1,15 +1,38 @@
 package eu.brevissimus.payment.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 public record CardDto(
-        @Schema(description = "Account Number", example = "12345678C") String accountNumber,
-        @Schema(description = "First Name", example = "Joch") String firstName,
-        @Schema(description = "Last Name", example = "Deo") String lastName,
-        @Schema(description = "Card Number", example = "Card Number") String cardNumber,
-        @Schema(description = "CCV Code", example = "123") Integer ccvCode,
-        @Schema(description = "Expiry Date", example = "01/26") LocalDate expiry
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "Account Number", requiredMode = REQUIRED, example = "12345678C")
+        String accountNumber,
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "First Name", requiredMode = REQUIRED, example = "Joch")
+        String firstName,
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "Last Name", requiredMode = REQUIRED, example = "Deo")
+        String lastName,
+        @NotBlank
+        @Size(max = 50)
+        @Schema(description = "Card Number", requiredMode = REQUIRED, example = "Card Number")
+        String cardNumber,
+        @NotBlank
+        @Schema(description = "CCV Code", requiredMode = REQUIRED, example = "123")
+        Integer ccvCode,
+        @NotNull
+        @PositiveOrZero
+        @Schema(description = "Expiry Date", requiredMode = REQUIRED, example = "01/26")
+        LocalDate expiry
 ) {
 }

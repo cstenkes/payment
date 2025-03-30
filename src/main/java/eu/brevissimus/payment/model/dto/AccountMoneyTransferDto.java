95605b3a24736a8be54695d989437b21a8da10a8
cstenkes <cs.tenkes@gmail.com>
2025-03-30 12:52:38 +0200

    PYM0002 - first validation added

--- src/main/java/eu/brevissimus/payment/model/dto/AccountMoneyTransferDto.java
+++ src/main/java/eu/brevissimus/payment/model/dto/AccountMoneyTransferDto.java
@@ -1,6 +1,5 @@
 package eu.brevissimus.payment.model.dto;
 
-import eu.brevissimus.payment.model.entity.Account;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
@@ -12,15 +11,5 @@ public record AccountMoneyTransferDto(
         @Schema(description = "To Account Number", example = "12345678B") String toAccountNumber,
         @Schema(description = "Transfer amount", example = "1000") BigDecimal amount,
         @Schema(description = "Transfer to currency", example = "CHF") String currency) {
-
-    public static AccountMoneyTransferDto of(Account fromAccount, Account toAccount, BigDecimal amount) {
-        return new AccountMoneyTransferDto(
-                LocalDateTime.now(),
-                fromAccount.getAccountNumber(),
-                toAccount.getAccountNumber(),
-                amount,
-                toAccount.getCurrency()
-        );
-    }
 
 }
--- src/main/java/eu/brevissimus/payment/model/dto/CustomerDto.java
+++ src/main/java/eu/brevissimus/payment/model/dto/CustomerDto.java
@@ -1,9 +1,20 @@
 package eu.brevissimus.payment.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CustomerDto(
-        @Schema(description = "First Name", example = "John") String firstName,
-        @Schema(description = "Last Name", example = "Doe") String lastName,
-        @Schema(description = "Address", example = "NYC First avenue 11") String address) {
+        @Schema(description = "First Name", example = "John")
+        @NotBlank
+        @Size(max = 50)
+        String firstName,
+        @Schema(description = "Last Name", example = "Doe")
+        @NotBlank
+        @Size(max = 50)
+        String lastName,
+        @NotBlank
+        @Size(max = 255)
+        @Schema(description = "Address", example = "NYC First avenue 11")
+        String address) {
 }

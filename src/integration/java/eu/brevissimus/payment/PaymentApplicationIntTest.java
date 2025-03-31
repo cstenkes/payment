ddf768814a612916f32d395bfa3b41ce0fc332b3
cstenkes <cs.tenkes@gmail.com>
2025-03-31 09:36:50 +0200

    PYM0004 - adding integration test

--- src/integration/java/eu/brevissimus/payment/PaymentApplicationIntTest.java
+++ src/integration/java/eu/brevissimus/payment/PaymentApplicationIntTest.java
@@ -1,33 +1,35 @@
 package eu.brevissimus.payment;
 
-import eu.brevissimus.payment.service.TransactionService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-//@SpringBootTest
-//@AutoConfigureMockMvc
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class PaymentApplicationIntTest {
 
-	//@Autowired
-	private TransactionService transactionService;
-
-	//@Autowired
+	@Autowired
 	private MockMvc mockMvc;
 
-	//@Test
-	void calendarServiceFreeTest() throws Exception {
+	@Test
+	void paymentServiceTest() throws Exception {
 		// given - setup or precondition
-        //paymentService.initDay(); // add fix day for test
+		// the full spring will boot up with init data via liquibase
 
 		// when - action
-		ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/payment/something"));
+		ResultActions response = mockMvc.perform(get("/payment/api/v1/cards/{card_number}/balance","12345678-12345678-12345678"));
 
 		// then - verify the output
 		response.andExpect(MockMvcResultMatchers.status().isOk());
-		//response.andExpect(MockMvcResultMatchers.jsonPath("$.calendarDays[0].day", CoreMatchers.is("2023-09-08")));
-		// ...
-
+		response.andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber", CoreMatchers.is("12345678A")));
+		response.andExpect(MockMvcResultMatchers.jsonPath("$.balance", CoreMatchers.is(500)));
+		response.andExpect(MockMvcResultMatchers.jsonPath("$.currency", CoreMatchers.is("EUR")));
 	}
 }
